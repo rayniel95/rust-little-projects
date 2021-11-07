cb54f0e84dc24d63056ca82d1d8b57aeb9626574
Rainyel <rainyel.ramos@gmail.com>
2021-11-07 17:11:43 -0500

    assert none in empty list

--- src/dsandalgo/linked_list/test.rs
+++ src/dsandalgo/linked_list/test.rs
@@ -93,6 +93,8 @@ mod test {
         let mut vector = vec![2, 5, 6, 3, 7, 10, 15];
         
         let mut list = LinkedList::<i32>::new();
+        assert_eq!(list.peek_first().is_none(), true);
+
         for element in &vector{
             list.add_last(*element);
         }
@@ -113,6 +115,7 @@ mod test {
 
         assert_eq!(index, vector.len());
         assert_eq!(list.count(), 0);
+        assert_eq!(list.peek_first().is_none(), true);
     }
 
     #[test]
