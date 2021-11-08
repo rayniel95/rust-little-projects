c6dd51f888defd53ec855e39d9541401f9927efd
Rainyel <rainyel.ramos@gmail.com>
2021-11-07 20:27:01 -0500

    added basics methods

--- src/dsandalgo/heap/heap.rs
+++ src/dsandalgo/heap/heap.rs
@@ -2,8 +2,27 @@ pub struct Heap<T>{
     backend: Vec<T>
 }
 
+// TODO - copy the array if it is not large enough
+
 impl<T> Heap<T> {
     pub fn new(capacity: u32)->Heap<T>{
-        
+
+    }
+
+    pub fn peek() -> &T{
+
+    }
+    pub fn pop_min()->T{
+
+    }
+    pub fn add(value: T, priority: u32){
+
+    }
+    pub fn set_priority(index: u32, priority: u32){
+
+    }
+
+    pub fn get_index(value: T)->Vec<u32>{
+
     }
 }
