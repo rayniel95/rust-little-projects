42f890a2a5eae0f195fdce8704605b9aecc4d0e1
Rainyel <rainyel.ramos@gmail.com>
2021-11-07 20:54:44 -0500

    new added

--- src/dsandalgo/heap/heap.rs
+++ src/dsandalgo/heap/heap.rs
@@ -1,33 +1,37 @@
+use std::usize;
+
 struct Cell<T>{
     value:T,
     priority: u32
 }
 
 pub struct Heap<T>{
-    backend: Vec<T>
+    backend: Vec<Cell<T>>
 }
 
 // TODO - copy the array if it is not large enough
-
+// NOTE - check the capacity and the lenght for modifications
 impl<T> Heap<T> {
-    pub fn new(capacity: u32)->Heap<T>{
-
+    pub fn new(capacity: usize)->Heap<T>{
+        Heap{
+            backend: Vec::with_capacity(capacity)
+        }
     }
 
-    pub fn peek() -> &T{
+    // pub fn peek() -> &T{
 
-    }
-    pub fn pop_min()->T{
+    // }
+    // pub fn pop_min()->T{
 
-    }
-    pub fn add(value: T, priority: u32){
+    // }
+    // pub fn add(value: T, priority: u32){
 
-    }
-    pub fn set_priority(index: u32, priority: u32){
+    // }
+    // pub fn set_priority(index: u32, priority: u32){
 
-    }
+    // }
 
-    pub fn get_index(value: T)->Vec<u32>{
+    // pub fn get_index(value: T)->Vec<u32>{
 
-    }
+    // }
 }
