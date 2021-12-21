b111daeff113aae1e08607dd1f04645a680de087
Rainyel Ramos <rainyel.ramos@gmail.com>
2021-12-20 19:00:54 -0500

    add fuzzy test

---
+++ src/dsandalgo/heap/test.rs
@@ -0,0 +1,32 @@
+use super::heap::HeapTree;
+use rand::Rng;
+
+#[cfg(test)]
+mod test {
+    use std::{cmp::Ordering, vec};
+
+    use super::*;
+
+    #[test]
+    fn fuzzy_test(){
+        let mut my_heap = HeapTree::<u32>::new();
+        let mut gen = rand::thread_rng();
+
+        for number in 0..100000{
+            let number = gen.gen::<u32>();
+            my_heap.add(number, number);
+        }
+
+        let mut min = my_heap.pop().unwrap();
+        for _ in 1..100000{
+            let mut new_min = my_heap.pop().unwrap();
+
+            if new_min < min{
+                println!("{:?} is grater than {:?}", min, new_min);
+                panic!();
+            }
+            min = new_min;
+        }
+
+    }
+}
