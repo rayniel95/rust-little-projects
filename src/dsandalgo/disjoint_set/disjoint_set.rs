baaaf9b15738bf11cc3df560eba9fbbcea37a96f
Rainyel Ramos <rainyel.ramos@gmail.com>
2021-12-24 20:59:31 -0500

    create disjoint set

---
+++ src/dsandalgo/disjoint_set/disjoint_set.rs
@@ -0,0 +1,52 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+type SetLink<T> = Rc<RefCell<Set<T>>>;
+
+trait SetLinkeable<T> {
+    fn find_set(&mut self)->Self;
+    fn merge(& mut self, other: &mut Self);
+}
+
+impl<T> SetLinkeable<T> for SetLink<T>{
+    fn find_set(&mut self) ->Self {
+        if let None = self.borrow().parent {
+            return Rc::clone(self);
+        }
+        self.borrow_mut().parent = Some(
+            self.borrow_mut().parent.as_mut().unwrap().find_set()
+        );
+        Rc::clone(self.borrow().parent.as_ref().unwrap())
+    }
+    fn merge(& mut self, other: &mut Self) {
+        let left = self.find_set();
+        let right=other.find_set();
+
+        if left.borrow().rank > right.borrow().rank{
+            (*right).borrow_mut().parent = Some(left);
+            return;
+        }
+
+        if left.borrow().rank == right.borrow().rank{
+            (*right).borrow_mut().rank += 1;
+        }
+        (*left).borrow_mut().parent = Some(right);
+
+    }
+}
+
+struct Set<T>{
+    value: T,
+    parent: Option<Rc<RefCell<Set<T>>>>,
+    rank: u32
+}
+
+impl<T> Set<T>{
+    pub fn new(value: T)-> Self{
+        Self{
+            value: value,
+            parent: None,
+            rank:0
+        }
+    }
+}
