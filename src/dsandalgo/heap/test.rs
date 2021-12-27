e5a9ba14b7e1eca3a98d0889d45f98349043b6ec
Rainyel Ramos <rainyel.ramos@gmail.com>
2021-12-26 20:15:01 -0500

    create a fuzzy test to test the add function

--- src/dsandalgo/heap/test.rs
+++ src/dsandalgo/heap/test.rs
@@ -7,26 +7,36 @@ mod test {
 
     use super::*;
 
+    // #[test]
+    // fn fuzzy_test(){
+    //     let mut my_heap = HeapTree::<u32>::new();
+    //     let mut gen = rand::thread_rng();
+
+    //     for number in 0..100000{
+    //         let number = gen.gen::<u32>();
+    //         my_heap.add(number, number);
+    //     }
+    //     print!("**********");
+    //     let mut min = my_heap.pop().unwrap();
+    //     for _ in 1..5000{
+    //         let mut new_min = my_heap.pop().unwrap();
+
+    //         if new_min < min{
+    //             println!("{:?} is grater than {:?}", min, new_min);
+    //             panic!();
+    //         }
+    //         min = new_min;
+    //     }
+        
+    // }
     #[test]
-    fn fuzzy_test(){
+    fn add_fuzzy_test(){
         let mut my_heap = HeapTree::<u32>::new();
         let mut gen = rand::thread_rng();
 
-        for number in 0..100000{
+        for _ in 0..10000{
             let number = gen.gen::<u32>();
             my_heap.add(number, number);
         }
-        print!("**********");
-        let mut min = my_heap.pop().unwrap();
-        for _ in 1..5000{
-            let mut new_min = my_heap.pop().unwrap();
-
-            if new_min < min{
-                println!("{:?} is grater than {:?}", min, new_min);
-                panic!();
-            }
-            min = new_min;
-        }
-        
     }
 }
