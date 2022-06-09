c47232e8dbb6d69684f058116369783f7e624ac4
Rainyel Ramos <rainyel.ramos@gmail.com>
2022-06-08 20:25:46 -0400

    add insertion

--- src/dsandalgo/linked_list_disjoint_set/linked_list_disjoint_set.rs
+++ src/dsandalgo/linked_list_disjoint_set/linked_list_disjoint_set.rs
@@ -6,7 +6,7 @@ struct NodeSet {
     next: Option<SetLink>,
     head: Option<SetLink>,
     length: u32,
-    index: usize
+    index: usize,
 }
 
 // TODO - implement drop
@@ -16,8 +16,8 @@ impl NodeSet {
         NodeSet {
             next: None,
             head: None,
-            length: 0,
-            index
+            length: 1,
+            index,
         }
     }
 }
@@ -54,26 +54,56 @@ impl DisjointSet {
         }
     }
 
-    fn find_set(&self, index: usize)->usize{
-        match self.array[index].borrow_mut().head.take(){
-            None=> index,
-            Some(first)=>{
+    fn find_set(&self, index: usize) -> usize {
+        match self.array[index].borrow_mut().head.take() {
+            None => index,
+            Some(first) => {
                 self.array[index].borrow_mut().head = Some(Rc::clone(&first));
                 first.borrow().index
             }
         }
     }
-    fn merge(&self, index1: usize, index2: usize){
+    fn merge(&self, index1: usize, index2: usize) {
         let one = self.find_set(index1);
         let two = self.find_set(index2);
 
-        if 
+        if one == two {
+            return;
+        }
 
         let lenght_one = self.array[one].borrow().length;
         let lenght_two = self.array[two].borrow().length;
 
-        if lenght_one > lenght_two{
+        if lenght_one > lenght_two {
             self.array[one].borrow_mut().length = lenght_one + lenght_two;
+            self.insert(one, two);
+            return;
+        }
+        self.array[two].borrow_mut().length = lenght_one + lenght_two;
+        self.insert(two, one);
+    }
+    fn insert(&self, one: usize, two: usize) {
+        let last = self.array[one].borrow_mut().next.take();
+        self.array[one].borrow_mut().next = Some(Rc::clone(&self.array[two]));
+        self.array[two].borrow_mut().length = 1;
+
+        let mut pointer = Rc::clone(&self.array[two]);
+        loop {
+            pointer.borrow_mut().head = Some(Rc::clone(&self.array[one]));
+            pointer.borrow_mut().length = 1;
+
+            let next = pointer.borrow_mut().next.take();
+            match next {
+                None => {
+                    pointer.borrow_mut().next = last;
+                    break;
+                }
+                Some(next_pointer) => {
+                    let temp = pointer;
+                    pointer = Rc::clone(&next_pointer);
+                    temp.borrow_mut().next = Some(Rc::clone(&pointer));
+                }
+            }
         }
     }
 }
