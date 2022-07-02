42dab95e41bfdc573525d8d4e2112ae5f5bcddef
Rainyel Ramos <rainyel.ramos@gmail.com>
2022-07-02 14:24:47 -0400

    fix matrix merge process

--- src/dsandalgo/linked_list_disjoint_set/test.rs
+++ src/dsandalgo/linked_list_disjoint_set/test.rs
@@ -10,40 +10,47 @@ mod test {
 
     #[test]
     fn fuzzy_test() {
-        let nodes = 1000;
-        let mut matrix = vec![vec![false; nodes]; nodes];
-        let disjoint_set = DisjointSet::new(nodes);
-        let times = 1000;
-        let mut generator = rand::thread_rng();
+        let nodes = 10000;
+        let times_to_merge = 5000;
 
-        for _ in 0..times {
+        let mut matrix = vec![vec![false; nodes]; nodes];
+
+        for index in 0..nodes {
+            matrix[index][index] = true;
+        }
+        let disjoint_set = DisjointSet::new(nodes);
+        let mut generator = rand::thread_rng();
+        println!("before merge");
+        for _ in 0..times_to_merge {
             let one = generator.gen_range(0..nodes);
             let two = generator.gen_range(0..nodes);
 
-            disjoint_set.merge(
-                one,
-                two
-            );
+            disjoint_set.merge(one, two);
 
             matrix[one][two] = true;
             matrix[two][one] = true;
 
-            for index in 0..nodes {
-                let expr = 
-                    matrix[two][index] || matrix[one as usize][index];
-
-                matrix[one][index] = expr;
-                matrix[two][index] = expr;
-            }         
+            for elem in 0..nodes {
+                if matrix[elem][one] || matrix[elem][two] {
+                    for other in 0..nodes {
+                        matrix[elem][other] = matrix[one][other] ||
+                            matrix[two][other]
+                    }
+                }
+            }
         }
-        for _ in 0..times {
+        println!("to check");
+        for _ in 0..times_to_merge {
             let one = generator.gen_range(0..nodes);
             let two = generator.gen_range(0..nodes);
 
             let one_repr = disjoint_set.find_set(one);
             let two_repr = disjoint_set.find_set(two);
 
-            assert_eq!(matrix[one][two], one_repr==two_repr);       
+            println!("one:{}-two:{}", one, two);
+            println!("one repr:{}-two repr:{}", one_repr, two_repr);
+
+            assert_eq!(matrix[one][two], one_repr == two_repr);
         }
     }
 }
