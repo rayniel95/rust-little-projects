8fbca8eabf890dbc52dfeeef925e1f57a7eba5ae
Rainyel Ramos <rainyel.ramos@gmail.com>
2022-07-03 18:06:54 -0400

    update findset

--- src/dsandalgo/off_line_minimum_problem/disjoint_set.rs
+++ src/dsandalgo/off_line_minimum_problem/disjoint_set.rs
@@ -1,43 +1,39 @@
 use std::cell::RefCell;
 use std::rc::{Rc, Weak};
 
-
 type StrongLink = Rc<RefCell<NodeSet>>;
 type WeakLink = Weak<RefCell<NodeSet>>;
 
 trait DisjointSetable {
-    fn find_set(&mut self)->Self;
-    fn merge(& mut self, other: &mut Self);
+    fn find_set(&mut self) -> Self;
+    fn merge(&mut self, other: &mut Self);
 }
 
-impl DisjointSetable for StrongLink{
-    fn find_set(&mut self) ->Self {
+impl DisjointSetable for StrongLink {
+    fn find_set(&mut self) -> Self {
         if let None = self.borrow().parent {
             return Rc::clone(self);
         }
-        self.borrow_mut().parent = Some(
-            self.borrow_mut().parent.as_mut().unwrap().find_set()
-        );
+        self.borrow_mut().parent = Some(self.borrow_mut().parent.as_mut().unwrap().find_set());
         Rc::clone(self.borrow().parent.as_ref().unwrap())
     }
-    fn merge(& mut self, other: &mut Self) {
+    fn merge(&mut self, other: &mut Self) {
         let left = self.find_set();
-        let right= other.find_set();
+        let right = other.find_set();
 
-        if left.borrow().rank > right.borrow().rank{
+        if left.borrow().rank > right.borrow().rank {
             (*right).borrow_mut().parent = Some(left);
             return;
         }
 
-        if left.borrow().rank == right.borrow().rank{
+        if left.borrow().rank == right.borrow().rank {
             (*right).borrow_mut().rank += 1;
         }
         (*left).borrow_mut().parent = Some(right);
-
     }
 }
 
-pub struct NodeSet{
+pub struct NodeSet {
     parent: Option<StrongLink>,
     rank: u32,
     index: usize,
@@ -46,11 +42,11 @@ pub struct NodeSet{
     next: Option<StrongLink>,
 }
 
-impl NodeSet{
-    fn new(index: usize)-> Self{
-        Self{
+impl NodeSet {
+    fn new(index: usize) -> Self {
+        Self {
             parent: None,
-            rank:1,
+            rank: 1,
             index,
             set_number: 1,
             previous: None,
@@ -65,22 +61,30 @@ impl Default for NodeSet {
     }
 }
 
-pub struct DisjointSet{
-    array: Vec<StrongLink>
+pub struct DisjointSet {
+    array: Vec<StrongLink>,
 }
 
-impl  DisjointSet{
-    pub fn new(size: usize)->Self{
-        Self{ 
-            array: (0..size).map(
-                |element| Rc::new(RefCell::new(NodeSet::new(element)))
-            ).collect()
+impl DisjointSet {
+    pub fn new(size: usize) -> Self {
+        Self {
+            array: (0..size)
+                .map(|element| Rc::new(RefCell::new(NodeSet::new(element))))
+                .collect(),
         }
     }
-    pub fn find_set(& mut self, index: usize)->usize{
-        self.array[index].find_set().borrow().index
+    pub fn find_set(&mut self, index: usize) -> usize {
+        if let None = self.array[index].borrow().parent {
+            return index;
+        }
+        let parent_index = 
+            self.array[index].borrow().parent.unwrap().borrow().index;
+
+        self.array[index].borrow_mut().parent =
+            Some(Rc::clone(&self.array[self.find_set(parent_index)]));
+        self.array[index].borrow().parent.unwrap().borrow().index
     }
-    pub fn merge(&self, index1: usize, index2: usize){
+    pub fn merge(&self, index1: usize, index2: usize) {
         let mut one = Rc::clone(&self.array[index1]);
         let mut two = Rc::clone(&self.array[index2]);
         one.merge(&mut two)
@@ -89,7 +93,7 @@ impl  DisjointSet{
 
 impl Drop for DisjointSet {
     fn drop(&mut self) {
-        for set in self.array.iter_mut(){
+        for set in self.array.iter_mut() {
             set.borrow_mut().parent = None;
         }
     }
