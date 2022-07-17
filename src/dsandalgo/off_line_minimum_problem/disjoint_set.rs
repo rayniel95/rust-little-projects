f69ba9748524993c47e4d69306434e5bb92f4458
Rainyel Ramos <rainyel.ramos@gmail.com>
2022-07-17 14:12:27 -0400

    fix reference error

--- src/dsandalgo/off_line_minimum_problem/disjoint_set.rs
+++ src/dsandalgo/off_line_minimum_problem/disjoint_set.rs
@@ -1,20 +1,25 @@
+use std::borrow::BorrowMut;
 use std::cell::RefCell;
 use std::rc::{Rc, Weak};
+
+use super::utils::{
+    Sequence, SequenceItem
+};
 
 type StrongLink = Rc<RefCell<NodeSet>>;
 type WeakLink = Weak<RefCell<NodeSet>>;
 
 pub struct NodeSet {
-    parent: Option<StrongLink>,
-    rank: u32,
-    index: usize,
-    set_number: u32,
-    previous: Option<WeakLink>,
-    next: Option<StrongLink>,
+    pub parent: Option<StrongLink>,
+    pub rank: u32,
+    pub index: usize,
+    pub set_number: u32,
+    pub previous: Option<WeakLink>,
+    pub next: Option<StrongLink>,
 }
 
 impl NodeSet {
-    fn new(index: usize) -> Self {
+    pub fn new(index: usize) -> Self {
         Self {
             parent: None,
             rank: 1,
@@ -62,7 +67,7 @@ impl DisjointSet {
         }
         let parent_index = array[index].borrow().parent.unwrap().borrow().index;
 
-        array[index].borrow_mut().parent = Some(Rc::clone(
+        (*array[index]).borrow_mut().parent = Some(Rc::clone(
             &array[DisjointSet::find_set(array, parent_index)],
         ));
         array[index].borrow().parent.unwrap().borrow().index
@@ -77,50 +82,78 @@ impl DisjointSet {
         }
 
         if array[one_repr].borrow().rank > array[two_repr].borrow().rank{
-            array[two_repr].borrow_mut().parent = Some(
+            (*array[two_repr]).borrow_mut().parent = Some(
                 Rc::clone(&array[one_repr])
             );
-            array[one_repr].borrow_mut().set_number = 
+            (*array[one_repr]).borrow_mut().set_number = 
                 array[two_repr].borrow().set_number;
 
-            array[two_repr].borrow_mut().previous.take();
+            (*array[two_repr]).borrow_mut().previous.take();
             let next = 
-                array[two_repr].borrow_mut().next.take();
+            (*array[two_repr]).borrow_mut().next.take();
 
-            array[one_repr].borrow_mut().next.take();
+            (*array[one_repr]).borrow_mut().next.take();
             match next {
                 None=>{},
                 Some(pointer)=>{
-                    pointer.borrow_mut().previous = Some(
+                    (*pointer).borrow_mut().previous = Some(
                         Rc::downgrade(&array[one_repr])
                     );
-                    array[one_repr].borrow_mut().next = Some(pointer);
+                    (*array[one_repr]).borrow_mut().next = Some(pointer);
                 }
             }
             return;
         }
         if array[one_repr].borrow().rank == array[two_repr].borrow().rank{
-            array[two_repr].borrow_mut().rank += 1;
+            (*array[two_repr]).borrow_mut().rank += 1;
         }
 
-        array[one_repr].borrow_mut().parent = Some(
+        (*array[one_repr]).borrow_mut().parent = Some(
             Rc::clone(&array[two_repr])
         );
-        array[one_repr].borrow_mut().next.take();
-        array[two_repr].borrow_mut().previous.take();
+        (*array[one_repr]).borrow_mut().next.take();
+        (*array[two_repr]).borrow_mut().previous.take();
 
         let previous =
-            array[one_repr].borrow_mut().previous.take();
+            (*array[one_repr]).borrow_mut().previous.take();
         match previous{
             None=>{},
             Some(pointer)=>{
                 let pointer_up = pointer.upgrade().unwrap();
-                pointer_up.borrow_mut().next = Some(
+                (*pointer_up).borrow_mut().next = Some(
                     Rc::clone(&array[two_repr])
                 );
-                array[two_repr].borrow_mut().previous = Some(pointer);
+                (*array[two_repr]).borrow_mut().previous = Some(pointer);
             }
         }
+    }
+
+    pub fn build_disjoint_set(sequence: &Sequence, n: usize)-> Self{
+        let mut array = (0..n).map(
+            |value| Rc::new(RefCell::new(NodeSet::new(value)))
+        ).collect::<Vec<Rc<RefCell<NodeSet>>>>();
+        let mut set_number = 1;
+        for elem in sequence.iter(){
+            match elem {
+                SequenceItem::E=>{set_number += 1;}
+                &SequenceItem::I(val)=>{
+                    (*array[val as usize]).borrow_mut().parent;
+                }
+            }
+        }
+
+        for (elem1, elem2) in sequence.iter().take(
+            sequence.len() - 1
+        ).zip(sequence.iter().skip(1)){
+            match (elem1, elem2){
+                (SequenceItem::I(val1), SequenceItem::I(val2))=>{
+                    DisjointSet::merge(array, val1, val2);
+                }
+                _=>{}
+            }
+        }
+
+
     }
     // pub fn merge(&self, index1: usize, index2: usize) {
     //     let left = self.find_set(index1);
